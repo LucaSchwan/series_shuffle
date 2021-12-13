10b3236d80b92f35f71530def8523ef5d8e02a6d
ehrenschwan <luca@ehrenschwan.com>
2021-12-13 16:27:38 +0100

    small improvements

--- frontend/app/components/navbar.js
+++ frontend/app/components/navbar.js
@@ -1,6 +1,4 @@
 import Component from '@glimmer/component';
-import { tracked } from '@glimmer/tracking';
-import Ember from 'ember';
 import { inject as service } from '@ember/service';
 import { action } from '@ember/object';
 
